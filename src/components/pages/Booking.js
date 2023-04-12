dee5f0a5365c46b356427c6bf4e20f7e48a34c92
Mikelesnr <mwanza.n.m@gmail.com>
2023-04-12 14:57:32 +0200

    react router dom installed

---
+++ src/components/pages/Booking.js
@@ -0,0 +1,11 @@
+import React,{useState,useEffect} from 'react';
+import {useNavigate} from 'react-router-dom';
+
+function Booking() {
+
+    return (
+         <h1>booking page</h1>
+    )
+}
+
+export default Booking;
